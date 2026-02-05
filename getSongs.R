78ca8ce28b6dccaa3881e204c5f994a29b6b2151
Laurent PELLET <laurentp@ispf.pf>
2026-02-04 15:39:21 -1000

    un export hebdo plus tout 2025

--- getSongs.R
+++ getSongs.R
@@ -6,9 +6,9 @@ URL <- sprintf("https://www.radiofrance.fr/fip/%s/api/songs", SOUSRADIO)
 OUTPUTFOLDER <- sprintf("output/%s", SOUSRADIO)
 
 ## APPELS API
-dt <- as.Date('2025-01-02')
-dt <- Sys.Date()
-for(i in 0:90) {
+dt <- as.Date('2026-01-31')
+#dt <- Sys.Date()
+for(i in 0:5) {
   cat(as.character(dt+i))
   DATA <- FIP.getSongForDay(dt+i)
   cat(nrow(DATA))
@@ -17,18 +17,22 @@ for(i in 0:90) {
 }
 
 ## CONSOLIDATIONS
-DATA <- unique(rbindlist(lapply(list.files(OUTPUTFOLDER, pattern="*.csv", full.names = T), fread), use.names = T, fill=T))
+DATA <- unique(rbindlist(lapply(list.files(OUTPUTFOLDER, pattern=".csv", full.names = T), fread), use.names = T, fill=T))
 DATA[,start:=as.POSIXct(start)]
 DATA[,end:=as.POSIXct(end)]
-saveRDS(DATA, file=sprintf("output/SONGS-%s-2024.rds",  SOUSRADIO))
+saveRDS(DATA, file=sprintf("output/SONGS-%s.rds",  SOUSRADIO))
+saveRDS(DATA[year(start)==2024], file=sprintf("output/SONGS-%s-2024.rds",  SOUSRADIO))
+saveRDS(DATA[year(start)==2025], file=sprintf("output/SONGS-%s-2025.rds",  SOUSRADIO))
+saveRDS(DATA[year(start)==2026], file=sprintf("output/SONGS-%s-2026.rds",  SOUSRADIO))
 
 ## ECRITURE D'UN FICHIER PAR MOIS
-DATA[, mois:=sprintf("%04d-%02d", year(start),month(start))]
-lstMois <- DATA[, .N, mois][,mois]
-lapply(lstMois, FUN=function(x) {
-  fwrite(DATA[mois==x,.(SOUNDIIZ=sprintf("%s - %s", Artist, Song))], sprintf("output/soundiiz/FIP-%s-%s.csv", SOUSRADIO, x))
-  #fwrite(DATA[mois==x & itunes!="",.(SOUNDIIZ=sprintf("%s - %s", Artist, Song))], sprintf("output/soundiiz/FIP-%s-%s.csv", SOUSRADIO, x))
-  
+DATA <- readRDS("output/SONGS-fip.rds")
+DATA[, YW :=sprintf("%04dW%02d", year(start),isoweek(start))]
+
+lapply(DATA[, unique(YW)], FUN=function(x) {
+  fwrite(
+    DATA[YW==x,.(SOUNDIIZ=sprintf("%s - %s", Artist, Song))],
+    sprintf("output/soundiiz/FIP-%s-%s.csv", SOUSRADIO, x))
 })
 
 ## ANALYSE
@@ -40,4 +44,13 @@ SONGS <- rbindlist(list(FIP, FIP.JAZZ, FIP.REGGAE), use.names = T , fill=T)
 FIP.REGGAE[, .N, Artist][order(-N)][1:20]
 FIP[, .N, Artist][order(-N)][1:20]
 
-DATA[, .N, Artist][order(-N)][1:20]
+DATA[year(start)==2026, .N, Artist][order(-N)][1:20]
+DATA[Year>=2020, .N, Artist][order(-N)][1:20]
+DATA[Artist=='Voyou', .N, Album]
+
+
+DATA[, .N, itunes][order(-N)][1:20]
+DATA[deezer=='https://www.deezer.com/track/2964593791']
+summary(DATA)
+
+
